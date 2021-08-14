eb822ab726401c39a86aa92ca81000a82548a758
stivencabrera3 <stiven.cabrera3@gmail.com>
2021-08-14 14:32:39 -0400

    start updateFilters

--- notes-functions.js
+++ notes-functions.js
@@ -1,4 +1,4 @@
-const renderNotes = (notes) => {
+const renderNotes = (notes, filters) => {
     notesContainer.innerHTML = '';
 
     notes.forEach(note => {
@@ -25,3 +25,25 @@ const createNote = () => {
         renderNotes(notes, filters);
     });
 };
+
+//todo: finish updateFilters:
+    // I've added an event listener to the searchbar. 
+    // It is listening for input and will fire every time user enters anything 
+    // into the search bar.
+
+    // Function should update the searchText key value located
+    // in the filters object in main.js (line 7)
+    // If user types 'groceries' into searchbar, filters.searchText should equal 'groceries', etc
+    // User input should be made lowercase for case insensitivity. 
+    // You also need to call renderTodos inside the event listener.
+    //  call updateFilters in main.js when fininshed
+
+
+//todo: add line in renderNotes() (line 1 in this file) that creates a filteredNotes array that filters the notes array for the notes that includes the searchText in the title OR body.
+    // make this case insensitive (toLowerCase)
+    // Replace the notes on line 4 of renderNotes with this filteredNotes array so that the function renders the filtered notes in DOM
+
+const updateFilters = () =>{
+    search.addEventListener('input', (e) =>{
+    });
+}
