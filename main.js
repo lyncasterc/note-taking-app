6f6fd9ada6b0a4209273c20771b329c321c77b7d
stivencabrera3 <stiven.cabrera3@gmail.com>
2021-08-22 20:48:37 -0400

    rename search to searchBar

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const deleteAllButton = document.querySelector('#delete-all');
 const newNoteForm = document.querySelector('#new-note-container');
 const notesContainer = document.querySelector('#note-card-container');
-const search = document.querySelector('#search-bar');
+const searchBar = document.querySelector('#search-bar');
 const notes = JSON.parse(localStorage.getItem('notes')) || [];
 
 const filters = {
